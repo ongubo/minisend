a8e10f100ffb5adec2b164931982885934855d65
ongubo <developerworks25@gmail.com>
2021-03-27 00:07:58 +0300

    Database Updates

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\models\Mail;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('status')->insert(
+            [
+                ['name' => 'Posted'],
+                ['name' => 'Sent'],
+                ['name' => 'Failed'],
+            ]
+        );
+
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            Mail::create([
+                'to' => $faker->unique()->safeEmail,
+                'from' => $faker->unique()->safeEmail,
+                'subject' => $faker->text($maxNbChars = 90),
+                'text_content' => $faker->text($maxNbChars = 400),
+                'html_content' => $faker->randomHtml(2,3) ,
+                'status_id' => $faker->numberBetween($min = 1, $max = 3),
+                'created_at' => $faker->dateTime($max = 'now', $timezone = null),
+            ]);
+        }
     }
 }
