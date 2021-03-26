69df03864a8d3c72eca0ac71d72a1dc9e61559fc
ongubo <developerworks25@gmail.com>
2021-03-27 00:08:30 +0300

    Manage routing

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::get('/emails', 'MailController@index');
+Route::post('/emails/store', 'MailController@store');
+Route::get('/emails/{id?}', 'MailController@show');
+// Route::post('/emails/update/{id?}', 'MailController@update');
+// Route::delete('/emails/{id?}', 'MailController@destroy');
